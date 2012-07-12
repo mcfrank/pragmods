26a735b39f76b9348a67fe2c2a8a0869f65ddfc9
cgpotts <cgpotts@ca805d18-53d8-a2c2-b6ae-b6f168819e88>
2012-07-12 01:48:01 +0000

    Continued improvement of search.R

--- search.R
+++ search.R
@@ -11,42 +11,19 @@ source('ibr.R')
 ## ncol: number of columns (properties/messages) to have
 ## include.row.repeats: allow identical entities (default: TRUE)
 ## include.col.repeats: allow identical messages/properties (default: TRUE)
-## include.empty: allow messages/properties true of no objects (defaut: TRUE, but this is incompatble with IBR and its variants)
-## include.universal: whether to include columns with all 1s (default: FALSE)
-## include.ineffable: whether to have any all 0s rows (default: FALSE)
+## include.col.empty: allow messages/properties true of no objects (defaut: TRUE, but this is incompatble with IBR and its variants)
+## include.col.universal: allow messages/properties true of all objects (defaut: TRUE)
+## include.row.empty: allow objects with no true messages/properties (default: TRUE)
+## include.row.universal:  allow objects with all messages/properties (default: TRUE)
 ##
 ## Value:
 ## mats: a list mapping integers to matrices.
 ##
-## For example,
-##
-## AllBinaryMatrices(2,2, include.empty=FALSE, include.universal=TRUE)
-##
-## [[1]]
-##    m1 m2
-## t1  1  0
-## t2  0  1
-##
-## [[2]]
-##    m1 m2
-## t1  1  1
-## t2  1  0
-##
-## Notice that
-##
-##    m1 m2
-## t1  0  1
-## t2  1  0
-##
-## is excluded. This is because it is a column permutation variant of [[1]]. Similarly,
-##
-##    m1 m2
-## t1  0  0
-## t2  1  1
-##
-## is excluded because it is a row permutation variant of [[2]].
 
-AllBinaryMatrices = function(nrow, ncol, include.row.repeats=TRUE, include.col.repeats=TRUE, include.empty=TRUE, include.universal=TRUE, include.ineffable=TRUE) {
+AllBinaryMatrices = function(nrow, ncol,
+  include.row.repeats=TRUE, include.col.repeats=TRUE,
+  include.col.empty=TRUE, include.col.universal=TRUE,
+  include.row.empty=TRUE, include.row.universal=TRUE) {
   total = nrow*ncol
   matcount = 2^total
   ## These are used to filter out matrices that are row-permutations of
@@ -71,40 +48,38 @@ AllBinaryMatrices = function(nrow, ncol, include.row.repeats=TRUE, include.col.r
     ## Canonical (permutation invariant) string versions:
     matstr.row = Matrix2CanonicalStr(thismat)
     matstr.col = Matrix2CanonicalStr(t(thismat))
-    ## If we've this matrix-type before, in terms of permutations on rows or columns,
-    ## then we register its canonical form for both but we don't do anything else:    
-    ##if (!matstr.row %in% row.matlib | !matstr.col %in% col.matlib) {
-    ##  col.matlib = c(col.matlib, matstr.col)
-    ## row.matlib = c(row.matlib, matstr.row)
-    ##}
-    ## If this is new in terms of permutations on both rows and columns:
-    ##else {
+    ## Iff we've never seen a row or column permutation variant of
+    ## this matrix, we process it:
+    if (!matstr.row %in% row.matlib & !matstr.col %in% col.matlib) {
       ## Add this matrix to the libraries:
       row.matlib = c(row.matlib, matstr.row)
       col.matlib = c(col.matlib, matstr.col)
       ## Exclude matrices that contain 0s columns, since the model
       ## is not able to recover from such situations:
-      if (include.empty | ContainsZerosCol(thismat) == FALSE) {
+      if (include.col.empty | ContainsZerosCol(thismat) == FALSE) {
         ## Option to exclude matrices in which a column has all 1s:
-        if (include.universal == TRUE | ContainsUniversalCol(thismat)==FALSE) {
+        if (include.col.universal == TRUE | ContainsUniversalCol(thismat)==FALSE) {
           ## Option to exclude matrices that contain all 0 rows:
-          if (include.ineffable == TRUE | ContainsZeroVector(thismat) == FALSE) {
-            ## Option to exclude matrices with repeated rows:
-            if (include.row.repeats == TRUE | ContainsRowRepeats(thismat) == FALSE) {
+          if (include.row.empty == TRUE | ContainsZerosRow(thismat) == FALSE) {
+            ## Option to exclude matrices that contain all 0 rows:
+            if (include.col.universal == TRUE | ContainsUniversalRow(thismat) == FALSE) {
               ## Option to exclude matrices with repeated rows:
-              if (include.col.repeats == TRUE | ContainsColRepeats(thismat) == FALSE) {
-                ## Get the corresponding columns from vecs:           
-                rownames(thismat) = row.names
-                colnames(thismat) = col.names
-                mats[[matind]] = thismat
-                ## Increment the matrix counter:
-                matind = matind + 1
+              if (include.row.repeats == TRUE | ContainsRowRepeats(thismat) == FALSE) {
+                ## Option to exclude matrices with repeated rows:
+                if (include.col.repeats == TRUE | ContainsColRepeats(thismat) == FALSE) {
+                  ## Get the corresponding columns from vecs:           
+                  rownames(thismat) = row.names
+                  colnames(thismat) = col.names
+                  mats[[matind]] = thismat
+                  ## Increment the matrix counter:
+                  matind = matind + 1
+                }
               }
             }
           }
         }
       }
-    ##}
+    }
     ## Increment while-loop counter:
     j = j + 1
   }

6dab231d169cb7ac413bf90f24d0eba94d2fbe94
michaelcfrank@gmail.com <michaelcfrank@gmail.com@ca805d18-53d8-a2c2-b6ae-b6f168819e88>
2012-08-14 18:44:17 +0000

    visualization of images

---
+++ trunk/viz_img.R
@@ -0,0 +1,81 @@
+
+## Used for displaying the raw matrix:
+library(plotrix)
+library(png)
+
+######################################################################
+## Visualize a matrix (any number of rows; at most three columns)
+## in terms of smiley faces based on pictures.
+
+ImageViz = function(m, print.matrix=FALSE) {
+  ## Error checking:
+  if (ncol(m) > 4) {
+    stop(paste("Apologies: I can't handle more than 4 columns/properties"))
+  }
+  ## Parameters for the plotting:
+  plot.width = nrow(m)*2
+  panels = nrow(m)
+  plot.height = 2
+  if (print.matrix) {
+    plot.width = plot.width + 2
+    panels = panels + 1
+  }
+  ## Plot the smileys:
+  dev.new(width=plot.width, height=plot.height)
+  par(mfrow=c(1,panels), oma=c(0,0,0,0), mar=c(0,0,0,0))
+  for (i in 1:nrow(m)) {
+    SmileyImg(m[i, ])
+  }
+  ## Add a final column for the matrix if requested:
+  if (print.matrix) {
+    ## Blank plot window:
+    plot(c(0,20), c(0,1), type='n', xlab='', ylab='', axes=F)
+    ## Add the matrix:
+    addtable2plot(0, 0.5, m, display.rownames=TRUE, bty='o', cex=0.8)
+  }  
+	
+}
+
+######################################################################
+## Generate smiley images based on a set of binary values:
+##
+## vals: a binary vector of length 0-3. For each coordinate, 0 means
+## absence of the property, and 1 means presence.
+##
+## vals[1]: add a hat
+## vals[2]: add glasses
+## vals[3]: add a mustache
+
+SmileyImg = function(vals=c(0,0,0,0)) {			
+  ## Basic locating parameters:
+  xval = 0
+  yval = 0
+  xlim = c(1,2)
+  ylim = c(1,2)  
+  
+  ## Make sure we have a 4-membered value list:
+  shortage = 4 - length(vals)
+  if (shortage > 0) {
+    vals = c(vals, rep(0, shortage))
+  }
+  
+  ## load features
+  feature.names <- c("tie","hat","mustache","glasses")
+  features <- list()
+  
+  for (i in 1:length(feature.names)) {
+  	features[[i]] <- readPNG(paste("stim/",feature.names[i],".png",sep=""))
+  }
+  
+  ## Start an empty plot window with the right dimensions:
+  plot(xlim, ylim, type='n', xlab='', ylab='', axes=F)  
+  base <- readPNG("stim/face.png")
+  rasterImage(base,1,1,2,2)
+  
+  for (i in 1:length(vals)) {
+  	if (vals[i]==1) {
+      		rasterImage(features[[i]],1,1,2,2)
+	}
+  }  
+}
+

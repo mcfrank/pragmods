c1beabf5e0d90c68e3ef90fd43fc3cb634464ea0
cgpotts <cgpotts@ca805d18-53d8-a2c2-b6ae-b6f168819e88>
2012-08-31 19:01:06 +0000

    consolidated experiment code

---
+++ createstims.R
@@ -0,0 +1,165 @@
+##library(jpeg)
+
+rm(list=ls())
+
+source('viz_img.R')
+##source('ibr.R')
+##source('agents.R')
+##source('search.R')
+
+######################################################################
+
+CreateExperimentalData = function(
+  expt=c(),
+  row.names=paste('r', seq(1:nrow(expt)), sep=''),
+  col.names=paste('m', seq(1:ncol(expt)), sep=''),
+  outputDirname=NULL,
+  outputFilename=NULL,
+  n.subs=50,
+  levels=c(),
+  target.referents=c(),
+  distractor.referents=c(),
+  other.referents=c(),
+  target.features=c(), 
+  stims=c(),
+  stim.plurals=c(),
+  stims.features=list(),
+  stims.features.word=list()  
+  ) {
+
+  ## A bit of error-checking; more could be done to ensure sensible
+  ## designs that will throw errors later.
+  if (is.null(outputDirname)) {
+    stop(paste('outputDirname must be specified'))
+  }
+  if (!file.exists(outputDirname)) {
+    stop(paste('The specified output directory', outputDirname, 'does not exist.'))
+  }
+  if (is.null(outputFilename)) {
+    stop('A filename argument is required.')
+  }
+
+  ## Just add intuitive row and column names for now:
+  expt = PrepExperimentalMatrix(expt)
+
+  ## The output data frame that we will store in CSV:
+  expt.data = data.frame(hitNum=1:n.subs)
+
+  ## Uniform column containing a flat string representation of the experimental matrix:
+  expt.data$originalMatrix = paste(expt, collapse="")
+
+  ## Row-iterator, filling out the rest of expt.data:
+  for (s in 1:n.subs) {
+
+    ## These shuffled indices are used to get random
+    ## selections from the materials:
+    stim.perm = randperm(1:length(stims))
+    level.perm = randperm(1:length(levels))
+
+    for (l in 1:length(levels)) {
+
+      ## Randomly chosen indices into the materials:
+      stim.index = stim.perm[l]
+      level.index = level.perm[l]
+
+      ## Current level:
+      level = levels[level.index]
+      
+      ## Permute the matrix randomly:
+      row.perm = randperm(1:nrow(expt))
+      col.perm = randperm(1:ncol(expt))
+      expt.perm = expt[row.perm, col.perm]
+      
+      ## Features of the current stimulus:      
+      stim = stims[stim.index]
+      stim.plural = stim.plurals[stim.index]
+      stim.features = stims.features[[stim.index]]
+      stim.features.word = stims.features.word[[stim.index]]
+      
+      ## Create the image filename and the image itself:
+      filename = StimFilename(s.index=s, level.index=l, outputDirname=outputDirname)
+      CreateStimImage(expt.perm, stim=stim, filename)
+
+      ## Cell values particular to this trial:
+      target.referent = match(target.referents[level.index], row.perm)
+      distractor.referent = match(distractor.referents[level.index], row.perm)      
+      other.referent = match(other.referents[level.index], row.perm)
+      target.feature = match(target.features[level.index], col.perm)
+
+      ## Add values for this row and level:
+      expt.data[s, IndexedColumnName("filenameTrial", l)] = filename
+      expt.data[s, IndexedColumnName("levelTrial", l)] = level
+      expt.data[s, IndexedColumnName("stimSingularTrial", l)] = stim
+      expt.data[s, IndexedColumnName("stimPluralTrial", l)] = stim.plural
+      expt.data[s, IndexedColumnName("targetReferentTrial", l)] = target.referent
+      expt.data[s, IndexedColumnName("distractorReferentTrial", l)] = distractor.referent		
+      expt.data[s, IndexedColumnName("otherReferentTrial", l)] = other.referent
+      expt.data[s, IndexedColumnName("targetFeatureTrial", l)] = stim.features[target.feature]
+
+      ## Columns recording particular details of this row:
+      num.with.features = colSums(expt)
+      c = 1
+      for (i in 1:ncol(expt)) {
+        if (num.with.features[col.perm[i]] > 0) {
+          expt.data[s, paste("check", c, "NameTrial", l, sep="")] = stim.features.word[i]
+          expt.data[s, paste("check", c, "CountTrial", l, sep="")] = num.with.features[col.perm[i]]
+          c = c + 1
+        }
+      }
+      expt.data[s, IndexedColumnName("targetReferentTrial", l)] = target.referent
+      expt.data[s, IndexedColumnName("permutedMatrix", l)] = paste(expt.perm,collapse="")
+    }
+  }
+
+  ## Create the output CSV file:  
+  write.csv(expt.data, file.path(outputDirname, outputFilename), row.names=FALSE)  
+}
+
+######################################################################
+## Shuffle a vector or matrix, with no change in dimensions.
+
+randperm <- function (x) {sample(x,length(x))}
+
+######################################################################
+## Add basic row and column labels to a matrix to help keep them
+## distinguished. Default is  (r1, r2, ..., rM) and (m1, m2, ..., mN),
+## but this can be changed with row.names and col.names keyword
+## arguments.
+
+PrepExperimentalMatrix = function(m,
+  row.names=paste('r', seq(1:nrow(m)), sep=''),
+  col.names=paste('m', seq(1:ncol(m)), sep='')) {
+  rownames(m) = row.names
+  colnames(m) = col.names
+  return(m)
+}
+
+######################################################################
+## Output image filename for stimuli:
+  
+StimFilename = function(s.index=NULL, level.index=NULL, outputDirname=NULL, extension=".jpg") {
+  filename = paste("S", as.character(s.index), "-trial", as.character(level.index), extension, sep="")
+  filename = file.path(outputDirname, filename)
+  return(filename)
+}
+
+######################################################################
+## Create an image file:
+
+CreateStimImage = function(expt.perm=NULL, stim=NULL, filename=NULL, height=2, width=8, units="in", res=144) {
+  jpeg(filename, height=height, width=width, units=units,res=res)
+  ImageViz(expt.perm, stim=stim)
+  dev.off()
+}
+
+######################################################################
+## Function for creating indexed column names in a uniform style:
+
+IndexedColumnName = function(label, index) {
+  return(paste(label, index, sep=""))
+}
+
+  
+
+
+  

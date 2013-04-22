935fcf8e2683d3a388ad6ba36992fb6d48e9ad59
michaelcfrank <michaelcfrank@gmail.com>
2013-04-22 19:18:31 +0000

    added v4 and v5 experiments

---
+++ pragmods_js/pragmods_v5-getResults.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+pushd /Applications/aws-mturk-clt-1.3.1/bin
+./getResults.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -successfile /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_js//pragmods_v5.success -outputfile /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_js//pragmods_v5.results.tsv
+popd

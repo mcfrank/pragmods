ed33255b2caa9a02891a4ec95802361881724375
michaelcfrank@gmail.com <michaelcfrank@gmail.com@ca805d18-53d8-a2c2-b6ae-b6f168819e88>
2013-12-03 05:04:45 +0000

    deletions

--- trunk/pragmods_js/pragmods_v1-getResults.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-pushd /Applications/aws-mturk-clt-1.3.1/bin
-./getResults.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -successfile /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_v1//pragmods_v1.success -outputfile /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_v1//pragmods_v1.results.tsv
-popd

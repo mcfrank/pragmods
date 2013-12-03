dc204ae0ab642954ef8c901dfcabfe9608a0d1f5
michaelcfrank <michaelcfrank@gmail.com>
2013-12-03 05:04:45 +0000

    deletions

--- pragmods_js/pragmods_v1-postHIT.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-pushd /Applications/aws-mturk-clt-1.3.1/bin
-./loadHITs.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -label /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_v1//pragmods_v1 -input /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_v1//pragmods_v1.input -question /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_v1//pragmods_v1.question -properties /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_v1//pragmods_v1.properties -maxhits 1
-popd
--- pragmods_js/pragmods_v7-postHIT.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-pushd /Applications/aws-mturk-clt-1.3.1/bin
-./loadHITs.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -label /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_js//pragmods_v7 -input /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_js//pragmods_v7.input -question /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_js//pragmods_v7.question -properties /Users/mcfrank/Projects/Pragmatics/pragmods/pragmods_js//pragmods_v7.properties -maxhits 1
-popd
